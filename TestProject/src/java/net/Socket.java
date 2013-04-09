6ffbbe811329bf9460ccfe8438842060ea6aae6e
pajdo.pajdo <pajdo.pajdo@gmail.com>
2013-04-09 22:09:31 +0200

    first test commit

---
+++ TestProject/src/java/net/Socket.java
@@ -0,0 +1,19 @@
+package java.net;
+
+public class Socket {
+	
+	
+	public String toString() {
+		System.out.println("custom toString");
+		return "CustomImpl";
+	}
+	
+	public void runMe() {
+		System.out.println("runme");
+	}
+	
+	public void doesNothing() {
+		
+	}
+
+}
---
+++ TestProject/src/temp/TestClass.java
@@ -0,0 +1,18 @@
+package temp;
+
+import java.net.InetAddress;
+import java.net.Socket;
+
+//import java.net.Inet4Address;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		Socket s = new Socket();
+
+		System.out.println(s.toString());
+
+	}
+
+}
